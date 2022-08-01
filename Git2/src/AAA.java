467c526e8fa7c4d44b5e5a465c4ae0a0ee12b046
Sony <Sony@DESKTOP-OJ3AVQH>
2022-08-01 18:16:16 +0530

    by Ashish Bhai

---
+++ Git2/src/AAA.java
@@ -0,0 +1,12 @@
+
+public class AAA {
+
+public static void main(String[] args) {
+		
+		System.out.println("This is Git Bhai");
+		
+		System.out.println("Maari Bhaai");
+		System.out.println("Kaat Daalungaaaaa");
+	}
+
+}
